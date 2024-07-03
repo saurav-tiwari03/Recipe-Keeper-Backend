a7ee6b42e0a35b283327b4eac48f06409321c505
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-07-03 15:06:10 +0530

    jwt token

---
+++ controllers/Auth.js
@@ -0,0 +1,80 @@
+const User = require('./../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async(req,res ) => {
+  try {
+    const {name,email,password} = req.body;
+    const userExists = await User.findOne({email});
+    if(userExists){
+      console.log('User already exists')
+      res.status(401).json({
+        success: false,
+        message:'User already exists'
+      })
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(password,10)
+    console.log('User signed up successfully')
+    const user = await User.create({name,email,password:hashedPassword});
+    let payload = {
+      id:user._id,
+      name:user.name,
+      email:user.email
+    }
+    let token = jwt.sign(payload,"saurav",{expiresIn:'1d'});
+    res.status(200).json({
+      success: true,
+      token:token,
+      message:"User signed up successfully"
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message:'Error while signing up'
+    })
+  }
+}
+
+exports.login = async(req,res) => {
+  try {
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      console.log('User not found')
+      res.status(404).json({
+        success: false,
+        message: 'User not found in the database'
+      });
+      return;
+    }
+    const isMatch = await bcrypt.compare(password,user.password );
+    if(isMatch){
+      const payload = {
+        id:user._id,
+        name:user.name,
+        email:user.email
+      }
+      let token = jwt.sign(payload,"saurav",{expiresIn:'1d'})
+      console.log('User logged in successfully')
+      res.status(200).json({
+        success:true,
+        token:token,
+        message: 'User logged in successfully'
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        message: 'Password does not match'
+      });
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message:'Error while logging in'
+    })
+  }
+}
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(express.json());
+app.use(cors())
 
 require('dotenv').config();
+const {connect} = require('./config/database')
+const router = require('./routes/route')
 
 const PORT = process.env.PORT || 8800
 app.listen(PORT,() => {
   console.log(`listening on ${PORT}`);
+  connect();
 })
+
+
 app.get('/',(req,res)  => {
-  return res.send(`
-  <h1>Hello world!</h1>`
-)
+  res.status(200).json({
+    message:'Welcome to Recipe-Keeper Backend'
+  })
 })
+
+app.use('/api/v1',router)
