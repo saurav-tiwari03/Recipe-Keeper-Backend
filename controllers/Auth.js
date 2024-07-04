6b351bef3072fcc33139bb0502fe58c3550e73fc
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-07-04 19:10:41 +0530

    user update

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 exports.signup = async(req,res ) => {
   try {
-    const {name,email,password} = req.body;
+    const {name,userName,email,password} = req.body;
     const userExists = await User.findOne({email});
     if(userExists){
       console.log('User already exists')
@@ -14,9 +14,18 @@ exports.signup = async(req,res ) => {
       })
       return;
     }
+    const userNameRegisterd = await User.findOne({userName});
+    if(userNameRegisterd){
+      console.log('Choose a unique username')
+      res.status(401).json({
+        success: false,
+        message:'Please enter a unique username'
+      })
+      return;
+    }
     const hashedPassword = await bcrypt.hash(password,10)
     console.log('User signed up successfully')
-    const user = await User.create({name,email,password:hashedPassword});
+    const user = await User.create({name,userName,email,password:hashedPassword});
     let payload = {
       id:user._id,
       name:user.name,
--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 const userSchema = new mongoose.Schema({
   name:{
+    type:String,
+    required:true
+  },
+  userName:{
     type:String,
     required:true
   },
@@ -12,6 +17,44 @@ const userSchema = new mongoose.Schema({
   password:{
     type:String,
     required:true
+  },
+  bio:{
+    type:String,
+    default:''
+  },
+  recipes:{
+    type:Array
+  },
+  upVotes:{
+    type:Number,
+    default:0
+  },
+  rating:{
+    type:Number,
+    default:0
+  }
+})
+
+userSchema.post("save",async function(doc) {
+  try {
+    console.log("Doc : ",doc)
+    let transporter = nodemailer.createTransport({
+      host:process.env.MAIL_HOST,
+      auth:{
+        user:process.env.MAIL_USER,
+        pass:process.env.MAIL_PASS
+      }
+    })
+
+    let info = await transporter.sendMail({
+      from:'Recipe Keeper',
+      to:doc.email,
+      subject:'Welcome to Recipe Keeper',
+      html:`<h1>Hello ${doc.name}</h1><h2>Recipe Keeper</h2> <p>Says hi</p> <a href='https://recipekkeeper.netlify.app/'>Recipe Keeper</a>`
+    })
+    console.log(info)
+  } catch (error) {
+    console.log(error)
   }
 })
 
